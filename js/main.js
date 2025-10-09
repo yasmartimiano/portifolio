23459e1097fbe6548b1304afad84dac816dda4bf
yasmartimiano <yasminliver56@gmail.com>
2025-10-09 10:24:34 -0300

    feat: criou observador que detecta quando elementos aparecem na tela e torna visível

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ const observerOptions = {
     threshold: 0.1,
     rootMargin: '0px 0px -50px 0px'
 };
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'translateY(0)';
+        }
+    });
+}, observerOptions);
